870d6b09f940f9f11fb0e7f910ed1ccc536dab2a
Felipe Maia <felipeoliveiramaia3@gmail.com>
2026-04-22 16:38:23 -0300

    fix: add allowed hosts for Railway and felipemaia.dev

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: [
+      'personal-site-felipe-production.up.railway.app',
+      'felipemaia.dev',
+      'www.felipemaia.dev',
+    ],
+  },
 });
